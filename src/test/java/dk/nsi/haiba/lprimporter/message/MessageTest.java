1b74fb6afe803ac81c009d4362d66fba9b646417
Aksel Schmidt <aks@trifork.com>
2014-02-12 13:59:43 +0100

    test of business derror log format

---
+++ src/test/java/dk/nsi/haiba/lprimporter/message/MessageTest.java
@@ -0,0 +1,58 @@
+/**
+ * The MIT License
+ *
+ * Original work sponsored and donated by National Board of e-Health (NSI), Denmark
+ * (http://www.nsi.dk)
+ *
+ * Copyright (C) 2011 National Board of e-Health (NSI), Denmark (http://www.nsi.dk)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package dk.nsi.haiba.lprimporter.message;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import dk.nsi.haiba.lprimporter.config.LPRTestConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+public class MessageTest {
+    @Configuration
+    @Import({ LPRTestConfiguration.class })
+    static class TestConfiguration {
+    }
+
+    @Autowired
+    MessageResolver messageResolver;
+
+    @Test
+    public void errorRuleMessage() throws Exception {
+        String message = messageResolver.getMessage("errorlog.rule.message", 1, 2, 3, 4);
+        assertEquals("LPR Recordnummer [1], Regel [2], fejlbesked [3], db [4]", message);
+    }
+}
